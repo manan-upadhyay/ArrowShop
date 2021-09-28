368a9089d4ea724c6728f9dab68bf24a40f84dff
Manan Upadhyay <manantheassassin@gmail.com>
2021-09-29 00:12:52 +0530

    Latest Products

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
+import products from "../products";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1> Latest Prodcuts</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
