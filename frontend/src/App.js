fca3ff3b459abccd4fb2167fbf1b91bffd7cce2a
Manan Upadhyay <manantheassassin@gmail.com>
2021-11-19 21:37:21 +0530

    Shipping Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 const App = () => {
   return (
     <Router>
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/profile" component={ProfileScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route exact path="/" component={HomeScreen} />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -37,8 +37,18 @@ const userInfoFromStorage =
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null;
 
+const shippingAddressFromStorage =
+  localStorage.getItem("shippingAddress") &&
+  localStorage.getItem("shippingAddress") !== undefined &&
+  localStorage.getItem("shippingAddress") !== null
+    ? JSON.parse(localStorage.getItem("shippingAddress"))
+    : {};
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
