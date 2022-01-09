a68a3aa152e150a8cea215e387cb1c41e1d3f98e
Manan Upadhyay <manantheassassin@gmail.com>
2022-01-09 11:07:06 +0530

    User Edit Backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,11 @@ import express from "express";
 import {
   authUser,
   deleteUser,
+  getUserById,
   getUserProfile,
   getUsers,
   registerUser,
+  updateUser,
   updateUserProfile,
 } from "../controllers/userController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
@@ -20,6 +22,10 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 
-router.route("/:id").delete(protect, admin, deleteUser);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
