622fa44fdf266b2f811756a764ca1ae27167cb18
Manan Upadhyay <manantheassassin@gmail.com>
2022-01-29 12:06:34 +0530

    Product List Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
 const App = () => {
   return (
     <Router>
@@ -34,6 +35,7 @@ const App = () => {
           <Route path="/cart/:id?" component={CartScreen} />
 
           <Route path="/admin/userlist" component={UserListScreen} />
+          <Route path="/admin/productlist" component={ProductListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
 
           <Route exact path="/" component={HomeScreen} />
