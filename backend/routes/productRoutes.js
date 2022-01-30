b8bede125b36a247d4e4ac2dc047beab96f864a8
Manan Upadhyay <manantheassassin@gmail.com>
2022-01-30 12:24:49 +0530

    Morgan and Review Path

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
@@ -16,5 +17,7 @@ router
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct);
+
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
